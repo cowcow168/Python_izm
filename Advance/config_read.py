fa2f7bf8419b52d5490ac0e7801ae512f6bf2962
ushijima <ushinokomoriuta@gmail.com>
2018-09-18 17:30:58 +0900

    ファイル読み込みメソッドの追加

---
+++ Advance/config_read.py
@@ -0,0 +1,16 @@
+#デフォルトであるモジュールと同一ファイル名の時は、読み込めない
+import configparser
+
+inifile = configparser.ConfigParser()
+inifile.read('config.ini','UTF-8')
+
+print(inifile.get('settings', 'host'))
+print(inifile.get('settings', 'port'))
+
+print(inifile.get('system', 'os'))
+print(inifile.get('system', 'version'))
+print(inifile.get('system', 'path'))
+
+print(inifile.get('user', 'name'))
+print(inifile.get('user', 'password'))
+print(inifile.get('user', 'mail'))
